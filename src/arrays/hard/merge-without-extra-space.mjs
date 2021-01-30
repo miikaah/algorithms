63eeb9395c1acd51e1cf5e932418867da40b7e76
Miika Henttonen <miika.henttonen@wunderdog.fi>
2021-01-30 16:38:50 +0200

    Better note for merge-without-extra-space

--- src/arrays/hard/merge-without-extra-space.mjs
+++ src/arrays/hard/merge-without-extra-space.mjs
@@ -59,27 +59,20 @@ const testCases = [
     },
 ];
 
-// Okay this uses some memory but it's a lot easier to read
-// no extra memory version below ->
+// Attempt 1: I read the instructions as don't use extra memory instead of space
+// which doesn't make any sense since javascript uses dynamic arrays
+// and doubles an array's space at 2, 4, 8 and so on and since you need _some_
+// memory for the for loop counters, but it made me come up with this algorithm that I find neat.
 const merge = (test) => {
     const { input } = test;
     const [arr1, arr2] = input;
     const n = arr1.length;
     const m = arr2.length;
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        // When the last element of arr1 is smaller than the first element of arr2 the work is complete
-        if (arr1[n - 1] < arr2[0]) {
-            break;
-        }
-
+    // When the last element of arr1 is smaller than the first element of arr2 the work is complete
+    while (arr1[n - 1] >= arr2[0]) {
         // Take the last element of arr1 and place it in the correct index in arr2
         for (let i = 0; i < m; i++) {
-            if (i + 1 === m) {
-                arr2.push(arr1.pop());
-                break;
-            }
             if (arr2[i] < arr1[n - 1] && arr2[i + 1] > arr1[n - 1]) {
                 arr2.splice(i + 1, 0, arr1.pop());
                 break;
@@ -105,45 +98,55 @@ testCases.forEach((test) => {
     assert.deepEqual(arr2, expected2);
 });
 
-// Version that doesn't used any extra memory
+// Attempt 2: If the constraint is that you can't increase the size of the arrays
+// the obvious solution is to use swap variables.
+//
+// How this is supposed to be a hard problem I don't understand
+// sinze this what I learned in the first C programming class I ever took
+// and it wasn't even in University...
+//
+// Then again it might be that I just didn't get this question.
+// Instructions unclear, carry on.
+//
+// P.S. That space restriction is pretty stupid since what are we working with here?
+// It's all in the memory anyway. ???
+//
+// P.P.S Ugh, I find this solution garbage compared to the elegance of the first one.
+// I'll trade performance for code that's easy to read any day of the week.
+const merge2 = (test) => {
+    const { input } = test;
+    const [arr1, arr2] = input;
+    const n = arr1.length;
+    const m = arr2.length;
+    let swap1, swap2;
 
-const mergeWithoutExtraSpace = ({ input }) => {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        // When the last element of arr1 is smaller than the first element of arr2 the work is complete
-        if (input[0][input[0].length - 1] < input[1][0]) {
-            break;
-        }
-
+    // When the last element of arr1 is smaller than the first element of arr2 the work is complete
+    while (arr1[n - 1] >= arr2[0]) {
         // Take the last element of arr1 and place it in the correct index in arr2
-        for (let i = 0; i < input[1].length; i++) {
-            if (i + 1 === input[1].length) {
-                input[1].push(input[0].pop());
-                break;
-            }
-            if (
-                input[1][i] < input[0][input[0].length - 1] &&
-                input[1][i + 1] > input[0][input[0].length - 1]
-            ) {
-                input[1].splice(i + 1, 0, input[0].pop());
+        for (let i = 0; i < m; i++) {
+            if (arr2[i] < arr1[n - 1] && arr2[i + 1] > arr1[n - 1]) {
+                swap1 = { index: i + 1, item: arr1.pop() };
                 break;
             }
         }
 
         // Take the first element of arr2 and place it in the correct index in arr1
-        for (let i = 0; i < input[1].length; i++) {
-            if (input[0][i] > input[1][0]) {
-                input[0].splice(i, 0, input[1].shift());
+        for (let i = 0; i < m; i++) {
+            if (arr1[i] > arr2[0]) {
+                swap2 = { index: i, item: arr2.shift() };
                 break;
             }
         }
+
+        arr2.splice(swap1.index, 0, swap1.item);
+        arr1.splice(swap2.index, 0, swap2.item);
     }
 
-    return input;
+    return [arr1, arr2];
 };
 
 testCases.forEach((test) => {
-    const [arr1, arr2] = mergeWithoutExtraSpace(test);
+    const [arr1, arr2] = merge2(test);
     const [expected1, expected2] = test.expected;
     assert.deepEqual(arr1, expected1);
     assert.deepEqual(arr2, expected2);
