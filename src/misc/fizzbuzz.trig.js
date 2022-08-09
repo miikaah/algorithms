61e1da2d3997519c45d0ff3422ab9244c0ce1d0e
Miika Henttonen <miika.henttonen@wunderdog.fi>
2022-08-09 11:39:46 +0300

    Pack trig more tightly

--- src/misc/fizzbuzz.trig.js
+++ src/misc/fizzbuzz.trig.js
@@ -3,9 +3,8 @@
 // for the multiples of five print “Buzz”. For numbers which are multiples
 // of both three and five print “FizzBuzz”.
 
-const tau = 2 * Math.PI;
+const isDivisibleBy = (denom, num) => Math.cos((num / denom) * 2 * Math.PI) === 1;
 
 for (let i = 1; i < 101; i++) {
-    // prettier-ignore
-    console.log(`${Math.cos((i / 3) * tau) === 1 ? "Fizz" : ""}${Math.cos((i / 5) * tau) === 1 ? "Buzz" : ""}` || i);
+    console.log(`${isDivisibleBy(3, i) ? "Fizz" : ""}${isDivisibleBy(5, i) ? "Buzz" : ""}` || i);
 }
